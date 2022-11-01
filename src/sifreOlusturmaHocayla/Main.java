90bababf2de31f45a99e1cfe17d1381e3925bc5f
Merve C <mervetascava@gmail.com>
2022-11-01 19:54:18 +0300

    Jav-2

---
+++ src/sifreOlusturmaHocayla/Main.java
@@ -0,0 +1,12 @@
+package sifreOlusturmaHocayla;
+
+public class Main {
+    public static void main(String[] args) {
+        EmailApp emailApp = new EmailApp("Garry","F");
+        //  System.out.println(emailApp.setDepartment());
+        System.out.println(emailApp.showInfo());
+
+    }
+
+    }
+
